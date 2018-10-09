51867a8125ed10f9f0bc61faf099dd8c98c225d2
slebaillif <slebaillif@gmail.com>
2018-10-09 13:56:57 +0100

    trying to add types

---
+++ src/app/service/fragment.ts
@@ -0,0 +1,6 @@
+import {Paragraph} from './paragraph';
+
+export interface DocumentFragment{
+    title:string;
+    paragraphs:Paragraph[];
+}
---
+++ src/app/service/paragraph.ts
@@ -0,0 +1,4 @@
+export interface Paragraph{
+    paragraph:string;
+    sentences:string[];
+}
